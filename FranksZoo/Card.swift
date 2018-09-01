51b991222f4f987d52218613cac727ec4991695e
Sweeper777 <sumulang@gmail.com>
2018-09-01 11:59:24 +0100

    added Card enum

---
+++ FranksZoo/Card.swift
@@ -0,0 +1,17 @@
+
+enum Card {
+    case whale
+    case elephant
+    case crocodile
+    case bear
+    case lion
+    case seal
+    case fox
+    case perch
+    case hedgehog
+    case fish
+    case mouse
+    case mosquito
+    case joker
+    
+}
