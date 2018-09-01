b76f589da55ed09a87459f1814ca7dd501fd1c16
Sweeper777 <sumulang@gmail.com>
2018-09-01 11:59:36 +0100

    added predatorDict

--- FranksZoo/Card.swift
+++ FranksZoo/Card.swift
@@ -14,4 +14,17 @@ enum Card {
     case mosquito
     case joker
     
+fileprivate let predatorDict: [Card: Set<Card>] = [
+    .elephant: [.mouse],
+    .crocodile: [.elephant],
+    .bear: [.whale, .elephant],
+    .lion: [.elephant],
+    .seal: [.bear, .whale],
+    .fox: [.elephant, .crocodile, .bear, .elephant],
+    .perch: [.whale, .crocodile, .bear, .seal],
+    .hedgehog: [.fox],
+    .fish: [.whale, .crocodile, .seal, .perch],
+    .mouse: [.crocodile, .bear, .lion, .seal, .fox, .hedgehog],
+    .mosquito: [.hedgehog, .fish, .mouse],
+]
 }
