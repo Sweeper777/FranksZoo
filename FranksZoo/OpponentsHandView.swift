369eb3940c70a6e3fcac828e5dbccbfd0da31311
Sweeper777 <sumulang@gmail.com>
2018-09-05 07:11:14 +0100

    calculated image centers for left and right case

--- FranksZoo/OpponentsHandView.swift
+++ FranksZoo/OpponentsHandView.swift
@@ -30,5 +30,21 @@ class OpponentsHandView : UIView {
             }
             imageViews.append(imageView)
         }
+        
+        var imageCenters = [CGPoint]()
+        switch orientation {
+        case .left, .right:
+            let x = width / 2
+            let cardHeight = width / 7 * 5
+            let offset = 10.f
+            let cardsHeight = cardHeight + offset * (numberOfCards.f - 1)
+            let top = height / 2 - cardsHeight / 2
+            let topCardCenter = top + cardHeight / 2
+            for i in 0..<numberOfCards {
+                imageCenters.append(CGPoint(x: x, y: topCardCenter + i.f * offset))
+            }
+        case .down:
+            break
+        }
     }
 }
