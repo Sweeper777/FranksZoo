8b519ba22f8724f0dcd2dd486a8056607baebeeb
Sweeper777 <sumulang@gmail.com>
2018-09-05 07:09:52 +0100

    added Orientation enum

--- FranksZoo/OpponentsHandView.swift
+++ FranksZoo/OpponentsHandView.swift
@@ -1,5 +1,11 @@
 import UIKit
 
 class OpponentsHandView : UIView {
+    @objc enum Orientation: Int {
+        case right
+        case down
+        case left
+    }
+    
     private var imageViews: [UIImageView] = []
 }
