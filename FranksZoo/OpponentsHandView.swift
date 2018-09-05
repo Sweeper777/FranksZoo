8d3bb404f9ee23cfaace9584aa91df9cff6dd962
Sweeper777 <sumulang@gmail.com>
2018-09-05 07:10:55 +0100

    created imageVIews

--- FranksZoo/OpponentsHandView.swift
+++ FranksZoo/OpponentsHandView.swift
@@ -19,5 +19,16 @@ class OpponentsHandView : UIView {
         subviews.forEach { $0.removeFromSuperview() }
         imageViews.removeAll()
         
+        for _ in 0..<numberOfCards {
+            let imageView = UIImageView()
+            imageView.image = #imageLiteral(resourceName: "flipside")
+            imageView.contentMode = .scaleToFill
+            if orientation == .down {
+                imageView.frame = CGRect(x: 0, y: 0, width: height / 7 * 5, height: height)
+            } else {
+                imageView.frame = CGRect(x: 0, y: 0, width: width / 7 * 5, height: width)
+            }
+            imageViews.append(imageView)
+        }
     }
 }
