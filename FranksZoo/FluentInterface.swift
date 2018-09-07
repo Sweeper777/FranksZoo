2b7f3197d2fbdf2e8e5b8659955f5f9f66bd3497
Sweeper777 <sumulang@gmail.com>
2018-09-07 06:38:21 +0100

    added Int extensions for half of the card types

--- FranksZoo/FluentInterface.swift
+++ FranksZoo/FluentInterface.swift
@@ -5,3 +5,46 @@ extension Move : ExpressibleByDictionaryLiteral {
         self.init(cards: Dictionary(uniqueKeysWithValues: elements))
     }
 }
+
+extension Int {
+    var whale: Move {
+        return [.whale: self]
+    }
+    
+    var whales: Move {
+        return [.whale: self]
+    }
+    
+    var elephant: Move {
+        return [.elephant: self]
+    }
+    
+    var elephants: Move {
+        return [.elephant: self]
+    }
+    
+    var crocodile: Move {
+        return [.crocodile: self]
+    }
+    
+    var crocodiles: Move {
+        return [.crocodile: self]
+    }
+    
+    var bear: Move {
+        return [.bear: self]
+    }
+    
+    var bears: Move {
+        return [.bear: self]
+    }
+    
+    var lion: Move {
+        return [.lion: self]
+    }
+    
+    var lions: Move {
+        return [.lion: self]
+    }
+    
+}
