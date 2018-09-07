f8b32e481ba17d24c444bb35a04493032b56f38c
Sweeper777 <sumulang@gmail.com>
2018-09-07 06:39:43 +0100

    added canMakeMove method

--- FranksZoo/Hand.swift
+++ FranksZoo/Hand.swift
@@ -5,4 +5,15 @@ struct Hand: Cards {
         self.cards = cards.filter { $0.value > 0 }
     }
     
+    func canMakeMove(_ move: Move) -> Bool {
+        for (key, value) in move.cards {
+            if cards[key] == nil {
+                return false
+            }
+            if cards[key]! < value {
+                return false
+            }
+        }
+        return true
+    }
 }
