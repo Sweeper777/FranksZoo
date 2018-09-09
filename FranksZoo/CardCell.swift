0bba702fddc9a1cea23ee9a0fd796ff5ee62815b
Sweeper777 <sumulang@gmail.com>
2018-09-09 09:55:52 +0100

    used transform to indicate selection instead

--- FranksZoo/CardCell.swift
+++ FranksZoo/CardCell.swift
@@ -8,8 +8,10 @@ class CardCell: UICollectionViewCell {
             if isSelected {
 //                imageView.layer.borderWidth = imageView.width * 0.07
 //                imageView.layer.borderColor = UIColor.yellow.cgColor
+                self.transform = .identity
             } else {
 //                imageView.layer.borderWidth = 0
+                self.transform = CGAffineTransform(translationX: 0, y: self.bounds.height * 0.1)
             }
         }
     }
