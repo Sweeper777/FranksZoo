2f19975782df2b0ba6e924d3cdff6c613ba19197
Sweeper777 <sumulang@gmail.com>
2018-09-09 09:55:33 +0100

    overridden isSelected

--- FranksZoo/CardCell.swift
+++ FranksZoo/CardCell.swift
@@ -3,4 +3,14 @@ import UIKit
 class CardCell: UICollectionViewCell {
     @IBOutlet var imageView: UIImageView!
     
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+//                imageView.layer.borderWidth = imageView.width * 0.07
+//                imageView.layer.borderColor = UIColor.yellow.cgColor
+            } else {
+//                imageView.layer.borderWidth = 0
+            }
+        }
+    }
 }
