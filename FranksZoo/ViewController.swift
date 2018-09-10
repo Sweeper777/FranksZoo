548f86d547fc0e96e9ef58a6c57d818948cc9388
Sweeper777 <sumulang@gmail.com>
2018-09-10 06:57:32 +0100

    implemented numberOfItemsInSection

--- FranksZoo/ViewController.swift
+++ FranksZoo/ViewController.swift
@@ -55,4 +55,8 @@ extension ViewController : UICollectionViewDataSource, UICollectionViewDelegateF
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cards.count
+    }
 }
