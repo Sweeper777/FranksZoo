fbcdd232c2ec571e3973c88a6b293f6d196f29e0
Sweeper777 <sumulang@gmail.com>
2018-09-11 07:03:41 +0100

    added currentTurn

--- FranksZoo/Game.swift
+++ FranksZoo/Game.swift
@@ -3,6 +3,14 @@ import SwiftyUtils
 class Game {
     let playerCount = 4
     var playerHands: [Hand]
+    var currentTurn = 0 {
+        didSet {
+            if currentTurn > playerCount - 1 {
+                currentTurn = 0
+            }
+        }
+    }
+    
     
     var lastMove: Move?
     var lastMoveMadeBy: Int?
