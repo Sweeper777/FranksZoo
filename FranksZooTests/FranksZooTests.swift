a677047be38a01534629fbb23179fa41e2e87ba2
Sweeper777 <sumulang@gmail.com>
2018-09-12 07:13:41 +0100

    added testCardsToArray

--- FranksZooTests/FranksZooTests.swift
+++ FranksZooTests/FranksZooTests.swift
@@ -8,4 +8,10 @@ class FranksZooTests: XCTestCase {
         XCTAssertEqual(Card.whale.predators, Set([]))
     }
     
+    func testCardsToArray() {
+        let testMove: Move = 5.elephants + 4.whales
+        let array = testMove.toArray()
+        XCTAssertEqual(array, [.whale, .whale, .whale, .whale, .elephant, .elephant, .elephant, .elephant, .elephant])
+    }
+    
 }
