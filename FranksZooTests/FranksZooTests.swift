92763e3ab0d1263c8c3c7bec1e8171025104ad5d
Sweeper777 <sumulang@gmail.com>
2018-09-13 07:05:10 +0100

    added testMoveIsLegal

--- FranksZooTests/FranksZooTests.swift
+++ FranksZooTests/FranksZooTests.swift
@@ -14,4 +14,17 @@ class FranksZooTests: XCTestCase {
         XCTAssertEqual(array, [.whale, .whale, .whale, .whale, .elephant, .elephant, .elephant, .elephant, .elephant])
     }
     
+    func testMoveIsLegal() {
+        var testMove = 1.elephant + 1.mosquito
+        XCTAssertTrue(testMove.isLegal)
+        testMove = 1.elephant + 1.joker + 2.mosquitoes
+        XCTAssertTrue(testMove.isLegal)
+        testMove = 2.mosquitoes + 1.elephant
+        XCTAssertFalse(testMove.isLegal)
+        testMove = 1.whale + 1.elephant
+        XCTAssertFalse(testMove.isLegal)
+        testMove = 1.joker
+        XCTAssertFalse(testMove.isLegal)
+    }
+    
 }
