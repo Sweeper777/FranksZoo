d117617a8d11569b811e84a111879a909cc15465
Sweeper777 <sumulang@gmail.com>
2018-09-16 10:05:09 +0100

    handled cards.count > 1 case

--- FranksZoo/MoveDisplayerView.swift
+++ FranksZoo/MoveDisplayerView.swift
@@ -24,5 +24,17 @@ class MoveDisplayerView: UIView {
         
         guard cards.count > 0 else { return }
         
+        if cards.count > 1 {
+            let totalWidth = cardSize.width * cards.count.f
+            let whitespace = self.width - totalWidth - 20
+            let separator = whitespace / (cards.count.f + 1)
+            for i in 0..<cards.count {
+                let image = UIImage(named: imageDict[cards[i]]!)
+                let x = 10 + (cardSize.width + separator) * i.f
+                let y = center.y - cardSize.height / 2
+                image?.draw(in: CGRect(origin: CGPoint(x: x, y: y), size: cardSize))
+            }
+        } else {
+        }
     }
 }
