b5f8d5f63a196c8865ccba11d56acd8cf09ac588
Sweeper777 <sumulang@gmail.com>
2018-10-01 17:30:12 +0100

    added Move.cardCount

--- FranksZoo/Move.swift
+++ FranksZoo/Move.swift
@@ -2,6 +2,8 @@ struct Move : Cards, Equatable {
     let cards: [Card : Int]
     static let pass = Move()
     
+    let cardCount: Int
+    
     init(cards: [Card: Int]) {
         self.cards = cards.filter { $0.value > 0 }
     }
