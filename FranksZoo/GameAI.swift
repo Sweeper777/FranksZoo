b8bbdc91ac2f6774c05aa1157020090faeda5d64
Sweeper777 <sumulang@gmail.com>
2018-10-02 07:20:20 +0100

    added weightDict

--- FranksZoo/GameAI.swift
+++ FranksZoo/GameAI.swift
@@ -1,4 +1,20 @@
 class GameAI {
     let game: Game
     let playerIndex: Int
+    
+    let weightDict: [Card: Int] = [
+        .joker: 0,
+        .elephant: 2,
+        .whale: 8,
+        .bear: 14,
+        .crocodile: 18,
+        .seal: 30,
+        .fox: 40,
+        .mouse: 46,
+        .lion: 48,
+        .hedgehog: 62,
+        .perch: 70,
+        .mosquito: 76,
+        .fish: 84
+    ]
 }
