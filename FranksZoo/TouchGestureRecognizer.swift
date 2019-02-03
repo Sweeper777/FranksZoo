f7a13e11afe5faf62ee11b53bf9d4aee13b79a96
Sweeper777 <sumulang@gmail.com>
2019-02-03 10:03:08 +0000

    Create TouchGestureRecognizer.swift

---
+++ FranksZoo/TouchGestureRecognizer.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class TouchGestureRecognizer : UIGestureRecognizer {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        if self.state == .possible {
+            self.state = .recognized
+        }
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
+        self.state = .failed
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
+        self.state = .failed
+    }
+}
