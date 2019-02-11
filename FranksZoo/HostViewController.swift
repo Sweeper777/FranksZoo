9cb34c7ce8accfd9fd516594260920182b4c14a5
Sweeper777 <sumulang@gmail.com>
2019-02-11 16:55:15 +0000

    added multipeer related properties

--- FranksZoo/HostViewController.swift
+++ FranksZoo/HostViewController.swift
@@ -8,5 +8,10 @@ class HostViewController : UIViewController {
     @IBOutlet var descriptionLabel: UILabel!
     @IBOutlet var tableView: UITableView!
     
+    let peerID = MCPeerID(displayName: UIDevice.current.name)
+    var session: MCSession!
+    var advertiser: MCNearbyServiceAdvertiser!
+    var connectedPeers: Variable<[MCPeerID]> = Variable([])
+    
     let disposeBag = DisposeBag()
 }
