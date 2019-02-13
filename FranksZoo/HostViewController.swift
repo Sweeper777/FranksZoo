81d91e47eb7e98397d4518fe24f5db3ec215bd69
Sweeper777 <sumulang@gmail.com>
2019-02-13 07:20:05 +0000

    added stubs for advertiser and session delegate methods

--- FranksZoo/HostViewController.swift
+++ FranksZoo/HostViewController.swift
@@ -89,3 +89,29 @@ class HostViewController : UIViewController {
         
     }
 }
+
+extension HostViewController : MCSessionDelegate, MCNearbyServiceAdvertiserDelegate {
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }
+    
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+    }
+    
+    
+}
