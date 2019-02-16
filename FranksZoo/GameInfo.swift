031735cf4d72fce4b05605487de0657cf43eb790
Sweeper777 <sumulang@gmail.com>
2019-02-16 19:29:36 +0000

    added coding keys

--- FranksZoo/GameInfo.swift
+++ FranksZoo/GameInfo.swift
@@ -4,4 +4,9 @@ struct GameInfo: Codable {
     let game: Game
     let playerOrder: [MCPeerID : Int]
     
+    enum CodingKeys: CodingKey {
+        case game
+        case playerOrder
+    }
+    
 }
