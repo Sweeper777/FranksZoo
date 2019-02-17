5d73ebc808805e1e434f042df50c8a79884fbfc2
Sweeper777 <sumulang@gmail.com>
2019-02-17 08:27:11 +0000

    added MCSessionDelegate extension for multipeer game VC

--- FranksZoo/MultipeerGameViewController.swift
+++ FranksZoo/MultipeerGameViewController.swift
@@ -295,3 +295,25 @@ extension MultipeerGameViewController : GameDelegate {
     }
 }
 
+extension MultipeerGameViewController : MCSessionDelegate {
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }
+    
+    
+}
