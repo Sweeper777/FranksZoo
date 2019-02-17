6e15f8bcc7abefd1dde56e3eed5417130585cffc
Sweeper777 <sumulang@gmail.com>
2019-02-17 08:16:33 +0000

    conformed to Encodable

--- FranksZoo/GameInfo.swift
+++ FranksZoo/GameInfo.swift
@@ -21,4 +21,10 @@ struct GameInfo: Codable {
         playerOrder = NSKeyedUnarchiver.unarchiveObject(with: playerOrderData) as! [MCPeerID : Int]
     }
     
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(game, forKey: .game)
+        let playerOrderData = NSKeyedArchiver.archivedData(withRootObject: playerOrder)
+        try container.encode(playerOrderData, forKey: .playerOrder)
+    }
 }
