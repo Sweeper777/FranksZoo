21d60f7ddc625e34512a408e10bce0ea66a66ee8
Sweeper777 <sumulang@gmail.com>
2019-02-27 07:16:24 +0000

    moved some methods to GameAI extension

--- FranksZoo/GameAI.swift
+++ FranksZoo/GameAI.swift
@@ -4,35 +4,7 @@ protocol GameAI {
     var playerIndex: Int { get }
 }
 
-class HeuristicAI : GameAI {
-    let game: Game
-    let playerIndex: Int
-    
-    let weightDict: [Card: Int] = [
-        .joker: 0,
-        .elephant: 2,
-        .whale: 8,
-        .bear: 14,
-        .crocodile: 18,
-        .seal: 30,
-        .fox: 40,
-        .mouse: 46,
-        .lion: 48,
-        .hedgehog: 62,
-        .perch: 70,
-        .mosquito: 76,
-        .fish: 84
-    ]
-    
-    var myHand: Hand {
-        return game.playerHands[playerIndex]
-    }
-    
-    init(game: Game, playerIndex: Int) {
-        self.game = game
-        self.playerIndex = playerIndex
-    }
-    
+extension GameAI {
     func allPossibleOpeningMoves(for hand: Hand) -> [Move] {
         var retVal = [Move]()
         for kvp in hand.cards {
@@ -57,10 +29,38 @@ class HeuristicAI : GameAI {
         }
     }
     
-    func isWinningMove(_ move: Move) -> Bool {
-        var hand = myHand
-        hand.makeMove(move)
-        return hand.isEmpty
+    var myHand: Hand {
+        return game.playerHands[playerIndex]
+    }
+}
+
+class HeuristicAI : GameAI {
+    let game: Game
+    let playerIndex: Int
+    
+    let weightDict: [Card: Int] = [
+        .joker: 0,
+        .elephant: 2,
+        .whale: 8,
+        .bear: 14,
+        .crocodile: 18,
+        .seal: 30,
+        .fox: 40,
+        .mouse: 46,
+        .lion: 48,
+        .hedgehog: 62,
+        .perch: 70,
+        .mosquito: 76,
+        .fish: 84
+    ]
+    
+    init(game: Game, playerIndex: Int) {
+        self.game = game
+        self.playerIndex = playerIndex
+    }
+    
+    }
+    
     }
     
     func isLosingMove(_ move: Move) -> Bool {
