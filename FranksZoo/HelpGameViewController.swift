0422c86ee5d447a343571692743c010065bbe356
Sweeper777 <sumulang@gmail.com>
2019-03-07 07:55:16 +0000

    overridden some methods

--- FranksZoo/HelpGameViewController.swift
+++ FranksZoo/HelpGameViewController.swift
@@ -6,4 +6,54 @@ class HelpGameViewController : GameViewControllerBase {
     var currentlyAllowedMove: Move?
     var currentHelpPart = 0
     
+    override func viewDidLoad() {
+        game = helpGame()
+        
+        game.delegate = self
+        
+        super.viewDidLoad()
+    }
+    
+    override func initialAnimationDidEnd() {
+        super.initialAnimationDidEnd()
+        
+        nextHelpPart()
+    }
+    
+    @IBAction override func dealPress() {
+        guard game.currentTurn == 0 && !game.ended else { return }
+        
+        let selectedCards = (handCollectionView.indexPathsForSelectedItems ?? []).map { cards[$0.item] }
+        if selectedCards.count > 0 {
+            let moveDict = Dictionary(grouping: selectedCards, by: { $0 }).mapValues { $0.count }
+            let move = Move(cards: moveDict)
+            
+            
+            let player = game.currentTurn
+            if (game.makeMove(move)) {
+                moveDisplayer.animateMove(move, forPlayer: player, completion: {
+                    [weak self] in
+                    self?.handCollectionView.reloadData()
+                    self?.updateOpponentsHandView()
+                    self?.updateMoveDisplayer()
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
+                        [weak self] in
+                        self?.nextHelpPart()
+                    })
+                })
+            }
+        }
+    }
+    
+    @IBAction override func passPress() {
+        guard game.currentTurn == 0 && !game.ended else { return }
+        game.makeMove(.pass)
+        moveDisplayer.animateMove(.pass, forPlayer: 0) {
+            [weak self] in
+            self?.handCollectionView.reloadData()
+            self?.updateOpponentsHandView()
+            self?.updateMoveDisplayer()
+        }
+    }
+    
 }
