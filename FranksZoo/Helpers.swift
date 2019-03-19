41ce568f9db9330cce9daa5d7ed02b3cd2a9bb7c
Sweeper777 <sumulang@gmail.com>
2019-03-19 18:15:16 +0000

    updated to Swift 4.2 in Helpers

--- FranksZoo/Helpers.swift
+++ FranksZoo/Helpers.swift
@@ -7,11 +7,11 @@ fileprivate func shouldContinueToEnlarge(targetSize: CGSize, currentSize: CGSize
 
 func fontSizeThatFits(size: CGSize, text: NSString, font: UIFont) -> CGFloat {
     var fontToTest = font.withSize(0)
-    var currentSize = text.size(withAttributes: [NSAttributedStringKey.font: fontToTest])
+    var currentSize = text.size(withAttributes: [NSAttributedString.Key.font: fontToTest])
     var fontSize = CGFloat(1)
     while shouldContinueToEnlarge(targetSize: size, currentSize: currentSize) {
         fontToTest = fontToTest.withSize(fontSize)
-        currentSize = text.size(withAttributes: [NSAttributedStringKey.font: fontToTest])
+        currentSize = text.size(withAttributes: [NSAttributedString.Key.font: fontToTest])
         fontSize += 1
     }
     return fontSize - 1
