d8c23e66e3356b4289f991f852598d08588f340d
Sweeper777 <sumulang@gmail.com>
2019-03-20 07:55:00 +0000

    added documentation comments to Move

--- FranksZoo/Move.swift
+++ FranksZoo/Move.swift
@@ -1,7 +1,12 @@
+/// An object that represents a Move in a game of Frank's Zoo
 struct Move : Cards, Equatable, Codable {
+    /// The cards that this move consists of
     let cards: [Card : Int]
+    
+    /// A move representing the move of not dealing any cards
     static let pass = Move()
     
+    /// The total number of cards in this move
     let cardCount: Int
     
     init(cards: [Card: Int]) {
@@ -17,6 +22,7 @@ struct Move : Cards, Equatable, Codable {
         return lhs.cards == rhs.cards
     }
     
+    /// Returns whether a move is a valid opening move
     var isLegal: Bool {
         // pass is always legal
         if self == .pass {
@@ -45,6 +51,7 @@ struct Move : Cards, Equatable, Codable {
         return cardCopy.keys.count == 1
     }
     
+    /// The main card type of the move
     var mainCardType: Card? {
         if !isLegal { return nil }
         
@@ -58,6 +65,7 @@ struct Move : Cards, Equatable, Codable {
         return cardsCopy.keys.first
     }
     
+    /// A set of moves that can defeat this move
     var defeatableMoves: [Move] {
         
         
@@ -72,6 +80,7 @@ struct Move : Cards, Equatable, Codable {
         return moves
     }
     
+    /// Returns whether this move can defeat another move
     func canDefeat(_ move: Move) -> Bool{
         guard self.isLegal && move.isLegal else {
             return false
@@ -101,6 +110,9 @@ struct Move : Cards, Equatable, Codable {
             (moveMainCardType.predators.contains(selfMainCardType) && selfCardCount == moveCardCount)
     }
     
+    /// Returns all the different sets of cards that can be used to represent a
+    /// certain number of a certain animal.
+    /// i.e. 2 fish can be represented by 2 fish, or 1 fish + 1 joker
     static func allVariants(cardType: Card, count: Int) -> [Move] {
         if count == 1 {
             return [[cardType: 1]]
